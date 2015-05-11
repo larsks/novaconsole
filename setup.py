796fe798b8f4959f71d1a1ad88f21bc0c135d813
Lars Kellogg-Stedman <lars@redhat.com>
2015-05-11 11:59:09 -0400

    inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     install_requires=open('requires.txt').readlines(),
-    version=2,
+    version='2.0.1',
     name='novaconsole',
     packages=['novaconsole'],
     entry_points={
